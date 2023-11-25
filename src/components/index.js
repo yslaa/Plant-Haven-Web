9fbc71acfcef2f006a29ca861830091737db11fe
yslaa <crishlan.fabila@tup.edu.ph>
2023-11-26 01:46:14 +0800

    Import components

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import AdminNavbar from "./Navbar/AdminNavbar";
+import EmployeeNavbar from "./Navbar/EmployeeNavbar";
+import CustomerNavbar from "./Navbar/CustomerNavbar";
+import Footer from "./Footer";
+import ProtectedRoute from "./ProtectedRoute";
+import UnprotectedRoute from "./UnprotectedRoute";
+import ImagePreview from "./ImagePreview";
+import ListData from "./ListData";
+
+export {
+    Navbar,
+    Footer,
+    ProtectedRoute,
+    UnprotectedRoute,
+    AdminNavbar,
+    EmployeeNavbar,
+    CustomerNavbar,
+    ImagePreview,
+    ListData,
+};
