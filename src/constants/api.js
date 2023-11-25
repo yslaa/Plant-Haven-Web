e6f65ed89f8ee3973980720829985f201f7791d9
yslaa <crishlan.fabila@tup.edu.ph>
2023-11-26 01:47:38 +0800

    Add api

---
+++ src/constants/api.js
@@ -0,0 +1,16 @@
+import TAGS from "./tags";
+
+export default {
+  GET: "GET",
+  POST: "POST",
+  PATCH: "PATCH",
+  PUT: "PUT",
+  DELETE: "DELETE",
+  TAGS: [
+    TAGS.USERS,
+    TAGS.DELIVERIES,
+    TAGS.PRODUCTS,
+    TAGS.TRANSACTIONS,
+    TAGS.COMMENTS,
+  ],
+};
