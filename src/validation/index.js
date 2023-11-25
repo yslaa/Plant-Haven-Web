c50d6ab2a5d336d089a70a03fdb0efdde98af7a0
yslaa <crishlan.fabila@tup.edu.ph>
2023-11-26 01:57:14 +0800

    Import validation

---
+++ src/validation/index.js
@@ -0,0 +1,32 @@
+
+import createCommentValidation from "./createComment";
+import createDeliveryValidation from "./createDelivery";
+import createProductValidation from "./createProduct";
+import createTransactionValidation from "./createTransaction";
+import createUserValidation from "./createUser";
+import editCommentValidation from "./editComment";
+import editDeliveryValidation from "./editDelivery";
+import editProductValidation from "./editProduct";
+import editTransactionValidation from "./editTransaction";
+import editUserValidation from "./editUser";
+import forgotPasswordValidation from "./forgotPassword";
+import loginUserValidation from "./loginUser";
+import resetPasswordValidation from "./resetPassword";
+import updatePasswordValidation from "./updatePassword";
+
+export {
+  createCommentValidation,
+  createDeliveryValidation,
+  createProductValidation,
+  createTransactionValidation,
+  createUserValidation,
+  editCommentValidation,
+  editDeliveryValidation,
+  editProductValidation,
+  editTransactionValidation,
+  editUserValidation,
+  forgotPasswordValidation,
+  loginUserValidation,
+  resetPasswordValidation,
+  updatePasswordValidation,
+};
