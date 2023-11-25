5917b40a50e9d1b77da70188e4837e8f2cf94f3a
yslaa <crishlan.fabila@tup.edu.ph>
2023-11-26 01:47:32 +0800

    Import constants

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+import TAGS from "./tags";
+import ROUTE from "./route";
+import API from "./api";
+import ROLES from "./roles";
+import USER from "./user";
+import STATUS from "./status";
+import VARIANT from "./variant";
+import MONTHS from "./months";
+
+export {
+  TAGS,
+  ROUTE,
+  API,
+  ROLES,
+  USER,
+  STATUS,
+  VARIANT,
+  MONTHS,
+};
