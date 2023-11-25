d24c9ad9b48f3eee7bf0409c9584d1cb30ad74ee
yslaa <crishlan.fabila@tup.edu.ph>
2023-11-26 01:47:59 +0800

    Add tags

---
+++ src/constants/tags.js
@@ -0,0 +1,14 @@
+export default {
+  API: "api",
+  AUTH: "auth",
+  USERS: "Users",
+  USER: "user",
+  PRODUCTS: "Products",
+  PRODUCT: "product",
+  TRANSACTIONS: "Transactions",
+  TRANSACTION: "transaction",
+  COMMENTS: "Comments",
+  COMMENT: "comment",
+  DELIVERIES: "Deliveries",
+  DELIVERY: "delivery",
+};
