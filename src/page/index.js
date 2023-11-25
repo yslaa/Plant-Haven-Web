347ce4eff424550e86056943006a6994f58689bd
yslaa <crishlan.fabila@tup.edu.ph>
2023-11-26 01:49:14 +0800

    Import page

---
+++ src/page/index.js
@@ -0,0 +1,69 @@
+import LoginUser from "./LoginUser";
+import CustomerWelcome from "./CustomerWelcome";
+import CustomerRegister from "./CustomerRegister";
+import EmployeeRegister from "./EmployeeRegister";
+import User from "./User";
+import UserGetById from "./User/UserGetById";
+import EditUser from "./User/EditUser";
+import UpdateUserInfo from "./User/UpdateUserInfo";
+import UpdateUserPassword from "./User/UpdateUserPassword";
+import ForgotPassword from "./ForgotPassword";
+import ResetPassword from "./ResetPassword";
+import Delivery from "./Delivery";
+import DeliveryGetById from "./Delivery/DeliveryGetById";
+import EditDelivery from "./Delivery/EditDelivery";
+import CreateDelivery from "./Delivery/CreateDelivery";
+import Product from "./Product";
+import ProductGetById from "./Product/ProductGetById";
+import EditProduct from "./Product/EditProduct";
+import CreateProduct from "./Product/CreateProduct";
+import Transaction from "./Transaction";
+import CreateTransaction from "./Transaction/createTransaction";
+import CartPreview from "./Transaction/CartPreview";
+import ProductList from "./Transaction/ProductList";
+import EditTransaction from "./Transaction/EditTransaction";
+import TransactionAll from "./Transaction/TransactionAll";
+import TransactionGetById from "./Transaction/TransactionGetById";
+import TransactionHistory from "./Transaction/transactionHistory";
+import Comment from "./Comment";
+import CommentGetById from "./Comment/CommentGetById";
+import EditComment from "./Comment/EditComment";
+import CreateComment from "./Comment/CreateComment";
+import CommentAll from "./Comment/CommentAll";
+import Dashboard from "./Dashboard";
+
+export {
+    LoginUser,
+    CustomerWelcome,
+    CustomerRegister,
+    EmployeeRegister,
+    UpdateUserInfo,
+    UpdateUserPassword,
+    ForgotPassword,
+    ResetPassword,
+    User,
+    UserGetById,
+    EditUser,
+    Delivery,
+    DeliveryGetById,
+    EditDelivery,
+    CreateDelivery,
+    Product,
+    ProductGetById,
+    EditProduct,
+    CreateProduct,
+    Transaction,
+    CreateTransaction,
+    CartPreview,
+    ProductList,
+    EditTransaction,
+    TransactionAll,
+    TransactionGetById,
+    TransactionHistory,
+    Comment,
+    CommentGetById,
+    EditComment,
+    CreateComment,
+    CommentAll,
+    Dashboard,
+};
