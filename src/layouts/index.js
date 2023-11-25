53678c90b0a7a5079af914b8f43d88d55957308f
yslaa <crishlan.fabila@tup.edu.ph>
2023-11-26 01:48:49 +0800

    Import Layouts

---
+++ src/layouts/index.js
@@ -0,0 +1,17 @@
+import RootLayout from "./RootLayout";
+import NotFound from "./NotFound";
+import Welcome from "./Welcome";
+import HomeLayout from "./HomeLayout";
+import AdminLayout from "./AdminLayout";
+import EmployeeLayout from "./EmployeeLayout";
+import CustomerLayout from "./CustomerLayout";
+
+export {
+    RootLayout,
+    NotFound,
+    Welcome,
+    HomeLayout,
+    AdminLayout,
+    EmployeeLayout,
+    CustomerLayout
+};
