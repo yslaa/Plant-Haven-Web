f1943f9fc6bc623e96aca0598c922147cf5f8b55
yslaa <crishlan.fabila@tup.edu.ph>
2023-11-26 01:57:03 +0800

    Add editUser

---
+++ src/validation/editUser.js
@@ -0,0 +1,9 @@
+import * as yup from "yup";
+
+export default yup.object({
+  name: yup.string("Enter your Name").required("Name is required"),
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+});
