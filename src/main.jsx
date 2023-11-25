bc8bc3d626e5a717500d872f7efaf3fca9377f9a
yslaa <crishlan.fabila@tup.edu.ph>
2023-11-26 01:46:06 +0800

    Add main

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./state/store";
+import { ToastContainer } from "react-toastify";
+import { PersistGate } from "redux-persist/integration/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <ToastContainer />
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
